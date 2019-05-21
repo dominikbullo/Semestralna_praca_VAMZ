ecf5e1fe875553f32fa6f3a0942a002d48ba3310
Dominik <dominobullo@gmail.com>
2019-05-21 23:42:24 +0200

    first try

--- app/src/main/java/com/example/semestralna_praca_vamz/MainActivity.kt
+++ app/src/main/java/com/example/semestralna_praca_vamz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.semestralna_praca_vamz
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView_main.setBackgroundColor(Color.BLUE)
+//        recyclerView_main.setBackgroundColor(Color.BLUE)
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+                        recyclerView_main.adapter = MainAdapter()
     }
 }
---
+++ app/src/main/java/com/example/semestralna_praca_vamz/MainAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.semestralna_praca_vamz
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class MainAdapter:RecyclerView.Adapter<CustomViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class CustomViewHolder(v: View):RecyclerView.ViewHolder(v){
+
+}
