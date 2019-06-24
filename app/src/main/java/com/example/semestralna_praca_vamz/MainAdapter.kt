659992c5768c5815faee5a1ee09e78b496537da6
Dominik <dominobullo@gmail.com>
2019-06-24 17:37:56 +0200

    disagn changes

--- app/src/main/java/com/example/semestralna_praca_vamz/MainAdapter.kt
+++ app/src/main/java/com/example/semestralna_praca_vamz/MainAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
@@ -24,6 +25,14 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolde
         val video = homeFeed.videos.get(position)
         holder.itemView.textView_video_title.text = video.name
         holder.itemView.textView_channel_name.text = video.channel.name
+
+        val thumbnailImageView = holder.itemView.imageView_thumbnail_video
+        Picasso.with(holder.itemView.context).load(video.imageUrl).into(thumbnailImageView)
+
+        val chanelProfileImageView = holder.itemView.imageView_chanel_profile
+        Picasso.with(holder.itemView.context).load(video.channel.profileImageUrl).into(chanelProfileImageView)
+
+
     }
 
 }
