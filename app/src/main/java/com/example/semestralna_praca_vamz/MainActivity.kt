3d878ad2beabcff0942e22ab68926b29a6e1c9e7
Dominik <dominobullo@gmail.com>
2019-06-24 16:10:48 +0200

    RecycleView done - part 1

--- app/src/main/java/com/example/semestralna_praca_vamz/MainActivity.kt
+++ app/src/main/java/com/example/semestralna_praca_vamz/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
 
 //        recyclerView_main.setBackgroundColor(Color.BLUE)
         recyclerView_main.layoutManager = LinearLayoutManager(this)
-                        recyclerView_main.adapter = MainAdapter()
+        recyclerView_main.adapter = MainAdapter()
     }
 }
--- app/src/main/java/com/example/semestralna_praca_vamz/MainAdapter.kt
+++ app/src/main/java/com/example/semestralna_praca_vamz/MainAdapter.kt
@@ -1,25 +1,32 @@
 package com.example.semestralna_praca_vamz
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.row.view.*
 
-class MainAdapter:RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+
+    val videoTitles = listOf("firts", "sec", "third")
 
     override fun getItemCount(): Int {
         return 3
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cellForRow = layoutInflater.inflate(R.layout.row, parent, false)
+        return CustomViewHolder(cellForRow)
     }
 
-    override fun onBindViewHolder(p0: CustomViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.itemView.textView_video_title.text = videoTitles.get(position)
+
     }
 
 }
 
-class CustomViewHolder(v: View):RecyclerView.ViewHolder(v){
+class CustomViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
 }
